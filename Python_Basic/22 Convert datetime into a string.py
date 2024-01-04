d92688a7ab47cc488575de06b9ff6a850e6acc77
7sumitjha <7sumitjha@gmail.com>
2024-01-05 00:17:57 +0530

    First commit

---
+++ Python_Basic/22 Convert datetime into a string.py
@@ -0,0 +1,7 @@
+#-----22 Convert datetime into a string -----
+from datetime import datetime
+
+given_date = datetime(2020, 2, 25)
+string_date = given_date.strftime("%Y-%m-%d %H:%M:%S")
+
+print(string_date)
